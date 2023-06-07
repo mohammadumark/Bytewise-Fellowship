a1a4be85883e91e36feaa7d44e4537389e359286
Mohammad Umar <128236219+mohammadumark@users.noreply.github.com>
2023-06-08 00:29:11 +0500

    daily

--- final_project/insta-clone/lib/main.dart
+++ final_project/insta-clone/lib/main.dart
@@ -1,33 +1,44 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:insta_clone/responsive/mobileScreenLayout.dart';
 import 'package:insta_clone/responsive/responsive_layout.dart';
 import 'package:insta_clone/responsive/webScreenLayoout.dart';
 import 'package:insta_clone/utils/colors.dart';
 
-void main()
-async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: 'AIzaSyBzVhVMQyeT8Tv4q_GmNS0WmHPuA8nFFBo',
+        appId: '1:790153239395:web:fd85e5dc9a100619c45156',
+        messagingSenderId: '790153239395',
+        projectId: 'insta-clone-20b38',
+        storageBucket: 'insta-clone-20b38.appspot.com',
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Instagram Clone',
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: mobileBackgroundColor,
       ),
-
-      home:  const ResponsiveLayout(
-          mobileScreenLayout: MobileScreenLayout(),
-          webScreenLayout: WebScreenLayout()),
+      home: const ResponsiveLayout(
+        mobileScreenLayout: MobileScreenLayout(),
+        webScreenLayout: WebScreenLayout(),
+      ),
     );
   }
 }
---
+++ final_project/insta-clone/lib/screens/login_screen.dart
@@ -0,0 +1,101 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:insta_clone/utils/colors.dart';
+import 'package:insta_clone/widgets/text_field_input.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 32),
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Flexible(child: Container(), flex: 2,),
+              SvgPicture.asset(
+                'assets/ic-instagram.svg',
+                color: primaryColor, height: 64,),
+              const SizedBox(height: 64),
+              TextFieldInput(
+                textEditingController: _emailController,
+                hintText: 'Enter your email',
+                textInputType: TextInputType.emailAddress,
+              ),
+              SizedBox(height: 24,),
+              TextFieldInput(
+                textEditingController: _passwordController,
+                hintText: 'Enter your password',
+                textInputType: TextInputType.text,
+                isPass: true,
+              ),
+              SizedBox(height: 24,),
+              Container(
+                child: const Text('Log in'),
+                width: double.infinity,
+                alignment: Alignment.center,
+                padding: const EdgeInsets.symmetric(vertical: 12),
+                decoration: const ShapeDecoration(
+                    shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(4)
+                    )
+                    )
+                ),
+                color: blueColor,
+              ),
+              const SizedBox(
+                height: 12,
+
+              ),
+              Flexible(flex: 2,child: Container(),),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 8,
+                    ),
+                    child: const Text('Dont have an account?'),
+                  ),
+                  Container(
+                    child: Text("Sign up",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),),
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 8
+                    ),
+                  )
+                ],
+
+              )
+
+
+            ],
+          ),
+        ),
+      ) ,
+    );
+  }
+}
